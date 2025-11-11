c94a133b5137989e076c6329ae7f3f6b69cc6029
Yangxmy <1580680232@qq.com>
2025-11-11 20:21:51 +0800

    Initial benchmarks

---
+++ BeyondAIME/config.py
@@ -0,0 +1,68 @@
+from agieval.core.run.runner_type import RunnerType
+from agieval.entity.eval_config import EvalConfig
+from agieval.entity.global_param import GlobalParam
+from agieval.common.param_utils import load_model_api_config
+from agieval.entity.flow_config import BenchmarkConfig, ContextParam, FlowStage, PluginConfig
+
+dataset = "BeyondAIME"
+
+eval_config: EvalConfig = EvalConfig(
+    runner=RunnerType.DATA_PARALLEL,
+    benchmark_config_template=True,
+    dataset_files=f"datasets/{dataset}",
+    benchmark_config="",
+    flow_config_file="",
+    work_dir=f"result/{dataset}",
+    data_parallel=10,
+    global_param=GlobalParam(),
+    plugin_param=ContextParam(
+        **load_model_api_config()
+    )
+)
+
+benchmark_config_template = BenchmarkConfig(
+    benchmark="",
+    location_test="",
+    use_cache=True,
+    flow_config_file="",
+    flow_stages=[
+        FlowStage(
+            plugin_implement="SimpleDataProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="GenerationScenario"
+                )
+            ]
+        ),
+        FlowStage(
+            plugin_implement="SimpleInferProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="LiteLLMModel"
+                ),
+                PluginConfig(
+                    plugin_implement="SingleRoundTextAgent"
+                )
+            ]
+        ),
+        FlowStage(
+            plugin_implement="ScoreInferProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="ScoreLiteLLMModel"
+                ),
+                PluginConfig(
+                    plugin_implement="ModelScoreZeroshotV3Agent"
+                )
+            ]
+        ),
+        FlowStage(
+            plugin_implement="SimpleMetricsProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="BBHMetrics",
+                )
+            ]
+        )
+    ]
+)
---
+++ GPQA/config.py
@@ -0,0 +1,68 @@
+from agieval.core.run.runner_type import RunnerType
+from agieval.entity.eval_config import EvalConfig
+from agieval.entity.flow_config import BenchmarkConfig, ContextParam, FlowStage, PluginConfig
+from agieval.entity.global_param import GlobalParam
+from agieval.common.param_utils import load_model_api_config
+
+benchmark_name = "GPQA"
+
+eval_config: EvalConfig = EvalConfig(
+    runner=RunnerType.DATA_PARALLEL,
+    benchmark_config_template=True,
+    dataset_files=f"datasets/{benchmark_name}/",
+    benchmark_config="",
+    flow_config_file="",
+    work_dir=f"result/{benchmark_name}",
+    data_parallel=2,
+    global_param=GlobalParam(),
+    plugin_param=ContextParam(
+        **load_model_api_config()
+    )
+)
+
+benchmark_config_template = BenchmarkConfig(
+    benchmark="",
+    location_test="",
+    use_cache=False,
+    flow_config_file="",
+    flow_stages=[
+        FlowStage(
+            plugin_implement="SimpleDataProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="GenerationScenario"
+                ),
+            ]
+        ),
+        FlowStage(
+            plugin_implement="SimpleInferProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="LiteLLMModel"
+                ),
+                PluginConfig(
+                    plugin_implement="SingleRoundTextAgent"
+                )
+            ]
+        ),
+        FlowStage(
+            plugin_implement="ScoreInferProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="ScoreOpenaiHttpModel"
+                ),
+                PluginConfig(
+                    plugin_implement="ModelScoreZeroshotV3Agent"
+                )
+            ]
+        ),
+        FlowStage(
+            plugin_implement="SimpleMetricsProcessor",
+            plugins=[
+                PluginConfig(
+                    plugin_implement="ModelEvalZeroshotV2Metrics"
+                ),
+            ]
+        )
+    ]
+)
